38916b295b990f29c397c784ee6c1503080baf80
CatgirlHelper <105621291+catgirlhelper@users.noreply.github.com>
2022-05-16 14:52:35 +0800

    Update zh-CN.js

--- zh-CN.js
+++ zh-CN.js
@@ -1,35 +1,50 @@
 var t1 =[];
 var t2 = [];
+t1.push("1. You obtain <strong>$PAW</strong> by staking <strong>CATGIRL-BNB pair</strong><br>2. $PAW will be generated on a <strong>算力预览</strong> <br>3. If you wish to <strong>Cancel &amp; Withdraw</strong> your Liquidity Pair Token, you must wait a cooldown period of <strong>14 days</strong> before you get your Liquidity Pair token back<br>4. If you try to withdraw more Liquidity Pair Token during the previous withdrawal cooldown period, the cooldown will be reset to <strong>14 days</strong><br>5. You may cancel your withdrawal at <strong>any time</strong> and continue to generate $PAW<br>6. There is a <strong>maximum</strong> $PAW balance you can hold, the formula is: <strong>MAX_BALANCE = PAW_PER_DAY * 30</strong><br><br><br><strong>DISCLAIMER: </strong> Even though this software has been thoroughly tested, this software's content and functionalities are still experimental. By using this software, you agree to hold catgirl.io harmless and not liable for any losses of the cryptocurrency assets or NFTs. Please use this software at your own risk");
+t2.push("1. 您可通过质押<strong>CATGIRL-BNB 流动性</strong>以获得<strong>$PAW</strong><br>2. $PAW 将每天<strong>自动生成</strong> <br>3. 如果您想<strong>取消质押并提取</strong>流动性，则必须提交请求后等待<strong>14 天</strong>后才可领取到账<br>4. 任何质押、取消质押操作进行后将重置<strong>14 天</strong>等待期<br>5. 您可以<strong>随时</strong>取消“解除质押”并重新继续自动获得 $PAW<br>6. $PAW的<strong>最大持有总量</strong>为： <strong>每日自动获取量 * 30</strong><br><br><br><strong>免责声明：</strong> 即使该应用已经过大量测试，但仍处于试验阶段。 使用本应用即表示您同意 catgirl.io 不对加密货币资产或 NFT 的任何损失承担责任。 请自行承担使用此应用的风险");
+
+t1.push("1. Only <strong>Rare Season Catgirls</strong> and<!-- --> <strong>above can farm</strong><br>2. <strong>Farming Power</strong> is based on the<!-- --> <strong>Rarity</strong> &amp; <strong>Nya Score</strong> of your provided Season Catgirl<br>3. Closing the slots and stopping the enhancement has a cooldown period of <strong>180 days</strong> from the time you initiated the withdrawal to receive your staked CATGIRL\u00A2 back<br>4. Your Season Catgirls will run out of <strong>energy</strong> <!-- -->after your first farming run. You will be required to spend<!-- --> <strong>$PAW</strong> to wake them back up. You can indicate if they are asleep based on a purple moon on their inventory slot<br>5. The <strong>Farming Reward</strong> is based on your portion of Farming Power compared to the Total Farming Power currently in the pool, and will be calculated on a daily basis. The Farming Reward will begin accumulating starting from the next day after you start farming<br>6. After you start Farming, there is a cooldown period of<!-- --> <strong>2 days</strong> before you can stop the farm. Please note that if you stop farming, there will be no Farming Reward, and any $PAW that was spent will not be refunded<br>7. You <u>must</u> claim your reward within<!-- --> <strong>30 days</strong> after the farming period <i>ends</i>, otherwise, your reward will contribute back to the farming pool<br><br><br><strong>*</strong>This is just an estimate. The amount earned per 24 hours may fluctuate based on the Total Farming Power currently in the pool<br><br><strong>DISCLAIMER: </strong> Even though this software has been thoroughly tested, this software's content and functionalities are still experimental. By using this software, you agree to hold catgirl.io harmless and not liable for any losses of the cryptocurrency assets or NFTs. Please use this software at your own risk")
+t2.push("1. 只有<strong>稀有</strong>及更高稀有度的猫娘<!-- --><strong>可质押至农场</strong><br>2. <strong>算力</strong>基于所质押猫娘的<!-- --> <strong>稀有度</strong> &amp; <strong>Nya 分</strong> 计算<br>3. 锁定插槽将取消插槽奖励，操作后需等待<strong>180 天</strong>才可领取所质押的 CATGIRL\u00A2 至账户<br>4. 每一次质押结束后猫娘将进入<strong>睡眠</strong> <!-- -->状态. 您需要花费<!-- --> <strong>$PAW</strong>唤醒猫娘. NFT上显示紫色月亮即代表处于睡眠状态<br>5. <strong>农场奖励</strong>基于您所质押的猫娘总算力与当前全网总算力, 每日自动进行计算. 农场奖励将自质押后的第二天开始累积<br>6. 开始质押后, 将有<!-- --> <strong>2 天</strong>时间可进行取消操作. 请注意，如果取消质押将不会获得任何奖励, 并且已花费的 $PAW 将不会退还<br>7. 您 <u>必须</u> 在<!-- --> <strong>30 天</strong>内领取农场奖励, 否则所有奖励将<i>退回</i>农场池！<br><br><br><strong>*</strong>预估值. 每日收益受链上数据影响而浮动计算<br><br><strong>免责声明：</strong> 即使该应用已经过大量测试，但仍处于试验阶段。 使用本应用即表示您同意 catgirl.io 不对加密货币资产或 NFT 的任何损失承担责任。 请自行承担使用此应用的风险")
+
 t1.push("NEW","SOON","Please connect your wallet","My NFTs","Disconnect","Home","Mystery Box","Farming","Staking","Nekofuse","Marketplace","Raffle","The Token","INFORMATION","Whitepaper","Audit","Liquidity Lock","LINKS","CONTRACT ADDRESS:","Theme Song");
 t2.push("新","即将上线","请连接钱包后使用","我的NFT","断开连接","首页", "盲盒","农场","流动性质押","合成","市场","抽奖","代币","更多信息","白皮书","审计","流动池锁定","链接","合约地址：","主题曲");
-t1.push("Collect, create, and engage with","Catgirls!","Supported exchanges to buy CATGIRL￠:","Ecosystem","Season Catgirls","Season One","CURRENT","The GENESIS Catgirls");
-t2.push("收集，创造并拥有","猫娘！","CATGIRL￠已上架交易所：","生态系统","已诞生猫娘","第 1 季","当前","创世季猫娘");
+
+t1.push("Collect, create, and engage with","Catgirls!","Supported exchanges to buy CATGIRL\u00A2:","Ecosystem","Season Catgirls","Season One","CURRENT","The GENESIS Catgirls");
+t2.push("收集，创造并拥有","猫娘！","CATGIRL\u00A2已上架交易所：","生态系统","已诞生猫娘","第 1 季","当前","创世季猫娘");
+
 t1.push("COMMON","RARE","EPIC","LEGENDARY","PAW-SOME");
 t2.push("普通","稀有","史诗","传奇","喵爪");
+
 t1.push("DIY Catgirls","Platform Stats","MYSTERY BOXES OPEN","CATGIRL NFT HOLDERS","CATGIRL HOLDERS","CHARACTERS &amp; GROWING");
-t2.push("个性化猫娘","平台统计","已销售盲盒","猫娘NFT持有地址","代币持有地址","猫娘角色数");
-t1.push("The sales for Season One of the Mystery Boxes are <strong>now closed!</strong>","Rarities &amp; Drop Rates","Rates slightly increased when buying with CATGIRL￠");
-t2.push("第一季盲盒已于2021年11月23日后<strong>停止销售！</strong>","稀有度 &amp; 掉率","使用 CATGIRL￠ 购买时掉率将增加");
+t2.push("个性化猫娘","平台统计","已销售盲盒","NFT持有地址","代币持有地址","猫娘角色数");
+
+
+t1.push("The sales for Season One of the Mystery Boxes are <strong>now closed!</strong>","Rarities &amp; Drop Rates","Rates slightly increased when buying with CATGIRL\u00A2");
+t2.push("第一季盲盒已于2021年11月23日后<strong>停止销售！</strong>","稀有度 &amp; 掉率","使用 CATGIRL\u00A2 购买时掉率将增加");
+
 t1.push("COMMON - 80.99%","RARE - 14.5%","EPIC - 3.5%","LEGENDARY - 1%","PAW-SOME - 0.01%","Obtainable Catgirls");
 t2.push("普通 - 80.99%","稀有 - 14.5%","史诗 - 3.5%","传奇 - 1%","喵爪 - 0.01%","躲在盲盒里的猫娘们");
+
 t1.push("Stake CATGIRL-BNB pair","Add Liquidity and Stake","Stake LP","INPUT","MAX","Staking Info","BNB Balance:","Catgirl Balance:","Withdraw Cooldown","Catgirl-BNB LP Balance:","PAW BALANCE");
 t2.push("质押 CATGIRL-BNB 流动性","添加流动性并质押","质押LP代币","输入","最大","质押说明","BNB 余额：","Catgirl 余额：","提取冷却","CATGIRL-BNB LP 余额：","$PAW 余额");
+
 t1.push("There will be a cooldown period of <b>14 days</b> if you wish to withdraw your Liquidity Pair Token. You may cancel the withdraw at any time and continue generating $PAW.","Estimated Paw per day*: ","Withdraw in Progress","Current Staked CATGIRL-BNB:");
 t2.push("如果您提取流动性代币，将有<b>14 天</b>的冷却期。 您可以随时取消提取并继续生成 $PAW。","预计每日可自动获得：","解除质押中","当前已质押LP代币：");
-t1.push("1. You obtain <strong>$PAW</strong> by staking <strong>CATGIRL-BNB pair</strong><br>2. $PAW will be generated on a <strong>daily basis</strong> <br>3. If you wish to <strong>Cancel &amp; Withdraw</strong> your Liquidity Pair Token, you must wait a cooldown period of <strong>14 days</strong> before you get your Liquidity Pair token back<br>4. If you try to withdraw more Liquidity Pair Token during the previous withdrawal cooldown period, the cooldown will be reset to <strong>14 days</strong><br>5. You may cancel your withdrawal at <strong>any time</strong> and continue to generate $PAW<br>6. There is a <strong>maximum</strong> $PAW balance you can hold, the formula is: <strong>MAX_BALANCE = PAW_PER_DAY * 30</strong><br><br><br><strong>DISCLAIMER: </strong> Even though this software has been thoroughly tested, this software's content and functionalities are still experimental. By using this software, you agree to hold catgirl.io harmless and not liable for any losses of the cryptocurrency assets or NFTs. Please use this software at your own risk");
-t2.push("1. 您可通过质押<strong>CATGIRL-BNB 流动性</strong>以获得<strong>$PAW</strong><br>2. $PAW 将每天<strong>自动生成</strong> <br>3. 如果您想<strong>取消质押并提取</strong>流动性，则必须提交请求后等待<strong>14 天</strong>后才可领取到账<br>4. 任何质押、取消质押操作进行后将重置<strong>14 天</strong>等待期<br>5. 您可以<strong>随时</strong>取消“解除质押”并重新继续自动获得 $PAW<br>6. $PAW的<strong>最大持有总量</strong>为： <strong>每日自动获取量 * 30</strong><br><br><br><strong>免责声明：</strong> 即使该应用已经过大量测试，但仍处于试验阶段。 使用本应用即表示您同意 catgirl.io 不对加密货币资产或 NFT 的任何损失承担责任。 请自行承担使用此应用的风险");
-t1.push("INVENTORY","FILTER","FARMING AREA","Lock","Unlock");
-t2.push("库存","筛选","农场区域","锁定","解锁");
+
+t1.push("Farming Info","INVENTORY","FILTER","FARMING AREA","Select Slot:","BONUS","Lock","Unlock","Farming Status","Pool Size","Total Farming Power","Total CATGIRL Locked","Set the Farming Period","7 DAYS","14 DAYS","-50% POWER","Farming Power","Estimated Earnings","Paw Price","PAW PRICE");
+t2.push("农场说明","库存","筛选","农场区域","选择插槽","奖励","锁定","解锁","农场状态","农场池","总算力","已质押","设置质押期限","7天","14天","-50% 算力","算力预览","收益预览","唤醒（$PAW）费用","共需$PAW：");
+
 t1.push("TIER 1 SLOT","TIER 2 SLOT","TIER 3 SLOT","TIER 4 SLOT","TIER 5 SLOT");
 t2.push("插槽1","插槽2","插槽3","插槽4","插槽5");
 
+t1.push("Try the Dark Side?")
+t2.push("尝试切换深色模式？")
+
 function d(){
-  var e = document.body.getElementsByTagName("*");
-  for(let i in e){
-    var j = t1.indexOf(e[i].innerHTML);
-    if(j!=-1){
-      e[i].innerHTML=t2[j];
-    }
-  }
+var e = document.body.getElementsByTagName("*");
+for(let i in e){
+var j = t1.indexOf(e[i].innerHTML);
+if(j!=-1){e[i].innerHTML=t2[j]}
+}
 }
 var t=self.setInterval("d()",1000);
